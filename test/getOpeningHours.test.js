ae87acc5bbf830c78c59a65694c294e6dda322eb
caren <carenoliveirasilvapontes@gmail.com>
2022-07-28 22:12:02 -0500

    requisito 6 e 7

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,12 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {});
+test('verifica , se passados os argumentos retorna mensagem de zoo fechado', () => {
+  expect(getOpeningHours('monday', '09:00-Am')).toEqual('The zoo is closed');
+});
+test('verifica , se passados os argumentos retorna mensagem de zoo aberto', () => {
+  expect(getOpeningHours('tuesday', '09:00-Am')).toEqual('The zoo is open');
+});
+test('verifica , se passados os argumentos retorna mensagem de zoo fechado', () => {
+  expect(getOpeningHours('wednesday', '09:00-Pm')).toEqual('The zoo is closed');
+});
