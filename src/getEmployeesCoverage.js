acc6d571d86829910e07189820eb8c4cb95230e9
caren <carenoliveirasilvapontes@gmail.com>
2022-07-29 17:59:19 -0500

    requisito11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function getAnimal(arr) {
+  return arr.map((element) => species
+    .find((animal) => animal.id === element).name);
 }
 
+function getLocation(arr) {
+  return getAnimal(arr).map((element) => species
+    .find((animal) => animal.name === element).location);
+}
+
+function allEmployees() {
+  return employees.map((employee) => ({ id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: getAnimal(employee.responsibleFor),
+    locations: getLocation(employee.responsibleFor) }));
+}
+
+function getEmployeesCoverage(obj) {
+  if (obj === undefined) {
+    return allEmployees();
+  }
+  const param = Object.values(obj).join('');
+  const getEmployee = employees.find((employee) => (
+    employee.id === param || employee.firstName === param || employee.lastName === param));
+  if (getEmployee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const { id, firstName, lastName, responsibleFor } = getEmployee;
+  return { id,
+    fullName: `${firstName} ${lastName}`,
+    species: getAnimal(responsibleFor),
+    locations: getLocation(responsibleFor) };
+}
 module.exports = getEmployeesCoverage;
