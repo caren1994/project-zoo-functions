83dde929eb4ce9c709445fdb61af41bc217b50b3
caren <carenoliveirasilvapontes@gmail.com>
2022-07-29 16:04:49 -0500

    requisito4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const isManagerfunc = isManager(managerId);
+  if (isManagerfunc === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const manager = employees.filter((element) => element.managers.includes(managerId)).map(
+    (element) => `${element.firstName} ${element.lastName}`,
+  );
+  return manager;
 }
 
 module.exports = { isManager, getRelatedEmployees };
