8cb0175a3a15242322801b52465d1af815891847
caren <carenoliveirasilvapontes@gmail.com>
2022-07-28 21:44:52 -0500

    requisito6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,27 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {});
+test('Caso não haja parâmetro ,retorna undefined',()=>{
+    expect(handlerElephants()).toBeUndefined();
+});
+test('verifica , se o parâmetro é uma string',()=>{
+    expect(handlerElephants(56)).toEqual('Parâmetro inválido, é necessário uma string');
+});
+test('verifica , se o parâmetro é uma string',()=>{
+    expect(handlerElephants(56)).toEqual('Parâmetro inválido, é necessário uma string');
+});
+test('verifica a quantidade da população dos elefantes',()=>{
+    expect(handlerElephants('popularity')).toBe(5);
+});
+test('verifica , se o parâmetro é uma string',()=>{
+    expect(handlerElephants('count')).toBe(4);
+});
+test('verifica , se existe um elefante com nome de jefferson',()=>{
+    expect(handlerElephants('names')).toContain('Jefferson');
+});
+test('verifica , se a média de idade é ',()=>{
+    expect(handlerElephants('averageAge')).toContain('10.5');
+});
+
+
+
