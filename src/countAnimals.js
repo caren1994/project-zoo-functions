1ed6c60fd7f027c9d8a4adfc6dab991598d7d7da
caren <carenoliveirasilvapontes@gmail.com>
2022-07-28 21:09:55 -0500

    requisito5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animais) {
+  if (!animais) {
+    return species.reduce((acc, objeto) => {
+      acc[objeto.name] = objeto.residents.length;
+      return acc;
+    }, {});
+  }
+  const { specie, sex } = animais;
+  const { residents } = species.find((animal) => animal.name === specie);
+  if (!sex) {
+    return residents.length;
+  }
+  return residents.filter((animal) => animal.sex === sex).length;
 }
+
+console.log(countAnimals({ specie: 'lions' }));
 
 module.exports = countAnimals;
