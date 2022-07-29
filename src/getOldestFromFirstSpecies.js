aca2013bf279fa3c02b541d5e78f4abcea668785
caren <carenoliveirasilvapontes@gmail.com>
2022-07-29 15:15:14 -0500

    requisito10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalPeloId = employees.find((pessoa) => pessoa.id === id)
+    .responsibleFor[0];
+  const animal1 = species.filter((animal) => animal.id === animalPeloId);
+  const maisVelho = animal1[0].residents.reduce((acc, curr) => {
+    if (acc.age > curr.age) {
+      return acc;
+    }
+    return curr;
+  });
+
+  const { name, sex, age } = maisVelho;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
